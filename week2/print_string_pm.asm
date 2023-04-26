2e3c8a30084a3ba19d8929c2b4dceec0c0a661c1
yuening <michelle.yn.zhao@gmail.com>
2023-04-25 18:54:33 -0700

    skeleton in progress

---
+++ week2/print_string_pm.asm
@@ -0,0 +1,6 @@
+; semicolons indicate comments
+; Goal: Create a 32-bit protected mode routine that writes directly to VGA
+
+[bits 32]
+; Define attributes
+; Modify print_string() to display string without using BIOS routine
